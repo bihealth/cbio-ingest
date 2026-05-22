1c95f0af54c9ee4389545f5ddc1f7924d73f0b19
Oliver Stolpe <oliver.stolpe@charite.de>
2026-05-22 13:24:34 +0200

    fix: minor fixes (#6)
    * applying db migrations in Dockerfile
    * updating makefile
    * updated pyproject and CI
    * adding install script.

--- docker/build.sh
+++ docker/build.sh
@@ -2,15 +2,11 @@
 
 set -e
 
-IMAGE=localhost:5000/cbio-ingest:dev
-
 echo "Building Docker image ..."
+
 docker build \
   -f docker/Dockerfile \
   -t $IMAGE \
   .
-
-echo "Pushing Docker image ..."
-docker push $IMAGE
 
 echo "Finished!"
